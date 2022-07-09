d1a232b51958d7d35bee0854da11614108dce68a
OFIRI <ofir937@gmail.com>
2022-07-09 11:25:27 +0300

    feat: add get book by id query & create stub functions for create, update, delete book.

--- models/book.js
+++ models/book.js
@@ -14,3 +14,19 @@ const books = module.export = mongoose.model("book", bookSchema);
 module.export.getAllBooks = (callback) => {
     books.find().exec(callback);
 };
+
+module.export.getBookById = (id, callback) => {
+    let query = { _id: id };
+    return (books.findOne(query).populate("books").exec(callback));
+};
+
+// create book
+module.export.createBook = (newBook, callback) => {
+    newBook.save(callback);
+};
+
+// update book by book id
+module.export.updateBook;
+
+// delete book by book id
+module.export.deleteBook;
