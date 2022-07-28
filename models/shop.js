30224317b9be1c21bf72d2df6103fc3dabfbdb2e
OFIRI <ofir937@gmail.com>
2022-07-28 15:27:28 +0300

    feat: create shop model

---
+++ models/shop.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const shopSchema = new Schema({
+    _id: { type: String, required: true },
+    location: {
+        city: { type: String },
+        street: { type: String },
+        coordinates: {
+            long: { type: String},
+            lat: { type: String }
+        }
+    }
+});
+
+export const Shop = mongoose.model("shop", shopSchema);
+
+// get all shops
+Shop.getAllShops = (callback) => {
+    console.log('im in get all shops inside model');
+    callback();
+    //Shop.find().exec(callback);
+};
