5bc6aa3ada3f95f1da60577d75448c4e5e85de78
OFIRI <ofir937@gmail.com>
2022-08-01 13:29:52 +0300

    feat: add create, update and delete shops to shops model & controller

--- controllers/shops.js
+++ controllers/shops.js
@@ -12,4 +12,79 @@ router.get('/', (req, res) => {
     });
 });
 
+// http get for /shops/:id
+router.get('/:id', (req, res) => {
+    Shop.getShopById(req.params.id, (err, shops) => {
+        if (err) return res.json(400, {
+            message: `Failed to load shop. Error: ${err}`
+         });
+        res.send(shops);
+    });
+});
+
+// http post for /shops/
+router.post('/', (req, res) => {
+    let newShop =  {
+        location: {
+            city: req.body.location?.city,
+            street: req.body.location?.street,
+            coordinates: {
+              long: req.body.location?.coordinates?.long,
+              lat: req.body.location?.coordinates?.lat,
+            },
+          },
+    };
+
+    Shop.createShop(newShop, (err, shop) => {
+        if (err) return res.status(400).json({
+            message: `Failed to create shop. Error: ${err}`
+         });
+
+        res.send(shop);
+    });
+});
+
+// http put for /shops/:id
+router.put('/:id', (req, res) => {
+    Shop.getShopById(req.params.id, (err, oldShop) => {
+        if (err) return res.json(400, {
+            message: `Failed to find shop. Error: ${err}`
+         });
+         console.log("oldShop")
+         console.log(oldShop)
+        let updatedShop = {
+            location: {
+                city: req.body.location?.city || oldShop.location?.city,
+                street: req.body.location?.street || oldShop.location?.street,
+                coordinates: {
+                  long: req.body?.location?.coordinates?.long || oldShop.location?.coordinates?.long,
+                  lat: req.body?.location?.coordinates?.lat || oldShop.location?.coordinates?.lat,
+                },
+            },
+        };
+
+        console.log("newShop")
+         console.log(updatedShop)
+    
+        Shop.updateShop(req.params.id, updatedShop, (err, shop) => {
+            if (err) return res.status(400).json({
+                message: `Failed to update shop. Error: ${err}`
+             });
+    
+            res.send(shop);
+        });
+    });
+});
+
+// http delete for /shops/:id
+router.delete('/:id', (req, res) => {
+    Shop.deleteShop(req.params.id, (err) => {
+        if (err) return res.json(400, {
+            message: `Failed to delete shop. Error: ${err}`
+         });
+
+         res.json({ success: true, message: `Shop deleted successfuly` });
+    });
+});
+
 export default router;
--- models/shop.js
+++ models/shop.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const shopSchema = new Schema({
   location: {
-    city: { type: String },
-    street: { type: String },
+    city: { type: String, required: true },
+    street: { type: String, required: true },
     coordinates: {
       long: { type: String },
       lat: { type: String },
@@ -17,4 +17,25 @@ export const Shop = mongoose.model("shop", shopSchema);
 // get all shops
 Shop.getAllShops = (callback) => {
   Shop.find().exec(callback);
+};
+
+// get shop by id
+Shop.getShopById = (id, callback) => {
+  Shop.findOne({ _id: id }).exec(callback);
+};
+
+// create shop
+Shop.createShop = (newShop, callback) => {
+  let shop = new Shop(newShop);
+  shop.save(callback);
+};
+
+// update shop by shop id
+Shop.updateShop = (shopId, updatedShop, callback) => {
+  Shop.findOneAndUpdate(shopId, updatedShop).exec(callback);
+};
+
+// delete shop by shop id
+Shop.deleteShop = (shopId, callback) => {
+  Shop.findOneAndDelete({ _id:shopId }).exec(callback);
 };
